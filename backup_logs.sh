b4d745afa1d84ad755b8dfceb6414a118ec51610
ogonzalez <om.gonzalez007@gmail.com>
2025-08-26 06:37:39 -0500

    moviendo archivo comprimido al directorio backup

--- backup_logs.sh
+++ backup_logs.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
-#Script make backup of the logs files
+#This Script make backup of the logs files
 #
+#Variables 
 pathdir=/var/log/
 logsdate="logs"-$(date +'%Y-%m-%d').tar.gz
 
-#empaquetando directorio 
-tar -czvf $logsdate $pathdir 
+#Compressing logs directory 
+echo "Generando el archivo de logs.."
+tar -czf $logsdate $pathdir 
+sleep 3
+echo "listo..!"
+
+#Creating BackUpDirectory
+if [ -d /backups ]
+then 
+    mv $logsdate /backups
+else
+   varDir=$(mkdir /backups)
+   mv $logsdate /backups
+fi
+
+
+
